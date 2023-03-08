7aa5c6ba3b5e987978565ed6d1017e5412cc7429
Skins85 <mark.skinsley@gmail.com>
2023-03-08 13:27:31 +0000

    Render all data and format position value

--- player-stats.js
+++ player-stats.js
@@ -10,63 +10,101 @@ export default function renderPlayerCard(url) {
             let players = data.players;
             
             players.map(function(p) {
-                console.log(p.stats);
+                const stats = p.stats;
+                console.log(p);
 
                 // Create elements
-                const li = document.createElement('li'),
-                    name = document.createElement('h2'),
-                    position = document.createElement('h3'),
-                    appearances = document.createElement('p'),
-                    goals = document.createElement('p'),
-                    assists = document.createElement('p'),
-                    goalsPerMatch = document.createElement('p'),
-                    passesPerMinute = document.createElement('p');
+                const card = document.createElement('div'),
+                      name = document.createElement('h2'),
+                      position = document.createElement('h3'),
+                      goalsPerMatch = document.createElement('p'),
+                      passesPerMinute = document.createElement('p'),
+                      select = document.createElement('select'),
+                      option = document.createElement('option');
 
-                // Assists check
-                let assistsTextNode;
-                const assistsResult = p.stats.filter(obj => {
-                    return obj.name === 'goal_assist'
-                })
-                if (assistsResult.length > 0) {
-                    assistsTextNode = document.createTextNode(assistsResult[0]['value']);
-                    assists.appendChild(assistsTextNode);
+                // Apply attributes to created elements
+                card.setAttribute('class', 'card card--player-stats');
+                select.setAttribute('name', 'players');
+                select.setAttribute('id', 'select_players')
+
+                // Create HTML for relevant piece of data
+                function createMarkUp(arr, id, tag, label) {
+                    const filteredResults = arr.filter(obj => {
+                        return obj.name === id
+                    })
+                    const el = document.createElement(tag);
+                    if (filteredResults.length > 0) {
+                        el.appendChild(document.createTextNode(`${label} ${filteredResults[0]['value']}`));
+                    } else {
+                        el.appendChild(document.createTextNode(`${label} 0`));
+                    }
+                    return el;
                 }
 
-                // Calculate values
-                let passesTotalValue = p.stats.find(p => p.name === 'fwd_pass').value + p.stats.find(p => p.name === 'backward_pass').value,
-                    minutesPlayedValue =  p.stats.find(p => p.name === 'mins_played').value,
-                    passesPerMinuteValue = (passesTotalValue / minutesPlayedValue).toFixed(2),
-                    goalsValue = p.stats.find(p => p.name === 'goals').value,
-                    appearancesValue = p.stats.find(p => p.name === 'appearances').value,
-                    goalsPerMatchValue = (goalsValue / appearancesValue).toFixed(2);
+                // Check data exists in feed
+                function checkData(arr, id) {
+                    const filteredResults = arr.filter(obj => {
+                        return obj.name === id
+                    })
+                    return filteredResults.length > 0 ? true : false;
+                }
+
+                // Values to be used in arithemtical operation; check to see they exist
+                const goalsExist = checkData(stats, 'goals'),
+                      appearancesExist = checkData(stats, 'appearances'),
+                      forwardPassesExist = checkData(stats, 'fwd_pass'),
+                      backwardPassesExist = checkData(stats, 'backward_pass'),
+                      minutesPlayedExist = checkData(stats, 'mins_played');
+
+                // Goals per match should only be calculated if both goals and appearances are present
+                if (goalsExist && appearancesExist) {
+                    const goalsPerMatchTextNode = document.createTextNode(`Goals per match ${(p.stats.find(p => p.name === 'goals').value / p.stats.find(p => p.name === 'appearances').value).toFixed(2)}`);
+                    goalsPerMatch.appendChild(goalsPerMatchTextNode)
+                }
+
+                // Passes can be either forward or back; at least one has to be present and a minutes played value
+                if ((forwardPassesExist || backwardPassesExist) && minutesPlayedExist) {
+                    let totalPasses = 0;
+                    
+                    forwardPassesExist ? totalPasses += p.stats.find(p => p.name === 'fwd_pass').value : null;
+                    backwardPassesExist ? totalPasses += p.stats.find(p => p.name === 'backward_pass').value : null;
+
+                    const passesPerMinuteTextNode = document.createTextNode(`Passes per minute ${(totalPasses / p.stats.find(p => p.name === 'mins_played').value).toFixed(2)}`);
+                    passesPerMinute.appendChild(passesPerMinuteTextNode);
+                }
+
+                // Convert single-letter positions into full word
+                let positionValue;
+                switch(p.player.info.position) {
+                    case 'D':
+                        positionValue = 'Defender';
+                        break;
+                    case 'M':
+                        positionValue = 'Midfielder';
+                        break;
+                    case 'F':
+                        positionValue = 'Forward';
+                        break;
+                    default:
+                        positionValue = 'Goalkeeper';
+                  }
 
                 // Create text nodes
                 let nameTextNode = document.createTextNode(`${p.player.name.first} ${p.player.name.last}`),
-                    positionTextNode = document.createTextNode(`${p.player.info.position}`),
-                    appearancesTextNode = document.createTextNode(`${appearancesValue}`),
-                    goalsTextNode = document.createTextNode(`${goalsValue}`),
-                    goalsPerMatchTextNode = document.createTextNode(goalsPerMatchValue),
-                    passesPerMinuteTextNode = document.createTextNode(passesPerMinuteValue);
+                    positionTextNode = document.createTextNode(`${positionValue}`);
+                
+                
+                const appearances =  createMarkUp(stats, 'appearances', 'p', 'Appearances'),
+                      assists = createMarkUp(stats, 'goal_assist', 'p', 'Assists'),
+                      goals = createMarkUp(stats, 'goals', 'p', 'Goals')
 
                 // Append text nodes
                 name.appendChild(nameTextNode);
                 position.appendChild(positionTextNode);
-                appearances.appendChild(appearancesTextNode);
-                goals.appendChild(goalsTextNode);
-                goalsPerMatch.appendChild(goalsPerMatchTextNode);
-                passesPerMinute.appendChild(passesPerMinuteTextNode);
 
-                // Append created elements
-                li.appendChild(name);
-                li.appendChild(position);
-                li.appendChild(appearances);
-                li.appendChild(goals);
-                li.appendChild(assists);
-                li.appendChild(goalsPerMatch);
-                li.appendChild(passesPerMinute)
+                card.append(...[name, position, appearances, goals, assists, goalsPerMatch, passesPerMinute])
 
-                ul.appendChild(li);
-
+                ul.appendChild(...[card]);
                 
             })
 
@@ -74,7 +112,5 @@ export default function renderPlayerCard(url) {
     .catch(function(error) {
         console.log(error);
     });
-
-    ul.appendChild(list);
 
 }
